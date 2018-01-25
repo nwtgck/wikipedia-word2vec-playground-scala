002b086d13c92312a0110ac7967c43cf204ac7db
Ryo Ota <nwtgck@gmail.com>
2018-01-26 00:57:16 +0900

    [add] Implement word2vec for Wikipedia Dump

--- build.sbt
+++ build.sbt
@@ -4,9 +4,12 @@ version := "0.1"
 
 scalaVersion := "2.11.12"
 
+val sparkVersion = "2.2.1"
+
 // Add dependency of `wikipedia-dump-loader` in GitHub
 // (from: https://github.com/sbt/sbt/issues/3489)
 dependsOn(RootProject(uri("git://github.com/nwtgck/wikipedia-dump-loader-scala.git#6c63b83782f1e3249c001235d5e6057b98ecad5e")))
 
 // https://mvnrepository.com/artifact/info.bliki.wiki/bliki-core
 libraryDependencies += "info.bliki.wiki" % "bliki-core" % "3.1.0"
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % sparkVersion
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,11 @@
 import java.io.{BufferedOutputStream, File, FileOutputStream, ObjectOutputStream}
 
+import info.bliki.wiki.filter.PlainTextConverter
+import info.bliki.wiki.model.WikiModel
 import org.apache.spark.sql.{Dataset, Encoders, SparkSession}
 import io.github.nwtgck.wikipedia_dump_loader.{Page, Redirect, Revision, WikipediaDumpLoader}
+import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
+import org.apache.spark.rdd.RDD
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -27,11 +31,77 @@ object Main {
       s"out/${xmlFileName}.ds.parquet"
     }
 
+    // Get Page Dataset
+    val pageDs: Dataset[Page] = loadPageDs(
+      sparkSession,
+      wikipediaPath = wikipediaPath,
+      parquetPath   = parquetPath
+    )
+
+    println(s"Page length: ${pageDs.count()}")
+
+
+    // Convert Dataset[Page] to plain text
+    val plainWikiDs: Dataset[String] =
+      pageDs
+        .map{page =>
+          for{
+            wikiText <- page.revision.textOpt
+          } yield new WikiModel("", "").render(new PlainTextConverter, wikiText)
+        }
+        .filter(_.isDefined)
+        .map(_.get)
+        .limit(1000) // TODO: HARD CODE: This reduces the number of pages
+        .cache()
+
+
+    println(s"plainWikiDs.cout: ${plainWikiDs.count()}")
+
+    // Create Words RDD
+    val wordsRdd: RDD[Seq[String]] =
+      plainWikiDs
+        .rdd
+        .map(text => text.split(" ").toSeq)
+        .cache()
+
+    // Create word2vec
+    val word2vec: Word2Vec = new Word2Vec()
+
+    // Learn
+    val word2VecModel: Word2VecModel = word2vec.fit(wordsRdd)
+
+    def printSynonyms(word: String): Unit = {
+      println(s"==== Synonym of '${word}' ====")
+      for(synonym <- word2VecModel.findSynonyms(word, 10)){
+        println(s"synonym: ${synonym}")
+      }
+    }
+
+    printSynonyms("the")
+    printSynonyms("it")
+    printSynonyms("of")
+    printSynonyms("America")
+    printSynonyms("Obama")
+
+
+
+  }
+
+  /**
+    * Load Dataset[Page] by Wikipedia Dump XML path
+    * @param sparkSession
+    * @param wikipediaPath
+    * @param parquetPath
+    * @return
+    */
+  def loadPageDs(sparkSession: SparkSession, wikipediaPath: String, parquetPath: String): Dataset[Page] = {
+
+    import sparkSession.implicits._
+
     // Get scheme of Page
     val pageScheme = Encoders.product[Page].schema
 
-    // Get Page Dataset
-    val pageDs: Dataset[Page] = if(new File(parquetPath).exists()){
+    if(new File(parquetPath).exists()){
 
       println(s"Load existing '${parquetPath}'")
 
@@ -55,8 +125,5 @@ object Main {
         .parquet(parquetPath)
       pageDs
     }
-
-    println(s"Page length: ${pageDs.count()}")
-
   }
 }
