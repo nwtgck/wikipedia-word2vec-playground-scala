617403a97d5b4b79e347b467b2daf8d7767c938b
Ryo Ota <nwtgck@gmail.com>
2018-01-27 15:45:15 +0900

    [change] Save word2vec model and load the model if it exists

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -30,50 +30,62 @@ object Main {
     // Import implicits
     import sparkSession.implicits._
 
-    // Create a Parquet directory path
-    val parquetPath: String = {
-      val xmlFileName: String = new File(wikipediaPath).getName
-      s"out/${xmlFileName}.ds.parquet"
-    }
-
-    // Get Page Dataset
-    val pageDs: Dataset[Page] = loadPageDs(
-      sparkSession,
-      wikipediaPath = wikipediaPath,
-      parquetPath   = parquetPath
-    )
-
-    println(s"Page length: ${pageDs.count()}")
-
-
-    // Convert Dataset[Page] to plain text
-    val plainWikiDs: Dataset[String] =
-      pageDs
-        .persist(StorageLevel.DISK_ONLY)
-        .map{page =>
-          for{
-            wikiText <- page.revision.textOpt
-          } yield new WikiModel("", "").render(new PlainTextConverter, wikiText)
-        }
-        .filter(_.isDefined)
-        .map(_.get)
-        .limit(pageLimit)
-
-
-    println(s"plainWikiDs.count: ${plainWikiDs.count()}")
-
-    // Create Words RDD
-    val wordsRdd: RDD[Seq[String]] =
-      plainWikiDs
-        .rdd
-        .persist(StorageLevel.DISK_ONLY)
-        .map(text => text.toLowerCase.replaceAll("[^a-zA-Z0-9\\s]", "").split(" ").toSeq) // (from: https://github.com/snowplow/spark-example-project/blob/3729ce01f01ce2024694e7622fc5a30d58d967bf/src/main/scala/com/snowplowanalytics/spark/WordCount.scala#L47)
-
     // Create word2vec
     val word2vec: Word2Vec = new Word2Vec()
 
-    // Learn
-    val word2VecModel: Word2VecModel = word2vec.fit(wordsRdd)
+    // Get word2vec model path
+    val word2vecModelPath: String = s"out/pagelimit${pageLimit}.word2vec_model"
+
+    // Get word2vec model
+    val word2VecModel = if(new File(word2vecModelPath).exists()){
+      // Load the existing model
+      Word2VecModel.load(sparkSession.sparkContext, word2vecModelPath)
+    } else {
+      // Create a Parquet directory path
+      val parquetPath: String = {
+        val xmlFileName: String = new File(wikipediaPath).getName
+        s"out/${xmlFileName}.ds.parquet"
+      }
+
+      // Get Page Dataset
+      val pageDs: Dataset[Page] = loadPageDs(
+        sparkSession,
+        wikipediaPath = wikipediaPath,
+        parquetPath   = parquetPath
+      )
+
+      println(s"Page length: ${pageDs.count()}")
+
+
+      // Convert Dataset[Page] to plain text
+      val plainWikiDs: Dataset[String] =
+        pageDs
+          .persist(StorageLevel.DISK_ONLY)
+          .map{page =>
+            for{
+              wikiText <- page.revision.textOpt
+            } yield new WikiModel("", "").render(new PlainTextConverter, wikiText)
+          }
+          .filter(_.isDefined)
+          .map(_.get)
+          .limit(pageLimit)
+
+
+      println(s"plainWikiDs.count: ${plainWikiDs.count()}")
+
+      // Create Words RDD
+      val wordsRdd: RDD[Seq[String]] =
+        plainWikiDs
+          .rdd
+          .persist(StorageLevel.DISK_ONLY)
+          .map(text => text.toLowerCase.replaceAll("[^a-zA-Z0-9\\s]", "").split(" ").toSeq) // (from: https://github.com/snowplow/spark-example-project/blob/3729ce01f01ce2024694e7622fc5a30d58d967bf/src/main/scala/com/snowplowanalytics/spark/WordCount.scala#L47)
+
+      // Learn
+      val word2VecModel: Word2VecModel = word2vec.fit(wordsRdd)
+      // Save the word2vec model
+      word2VecModel.save(sparkSession.sparkContext, word2vecModelPath)
+      word2VecModel
+    }
 
     // Print synonyms to stdout
     def printSynonyms(word: String): Unit = {
