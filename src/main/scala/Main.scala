78d7255ede1871a3261dcaf86f133f1e631726ee
Ryo Ota <nwtgck@gmail.com>
2018-01-27 15:10:58 +0900

    [change] Change the way of caching and the way of tokenize

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,12 @@
-import java.io.{BufferedOutputStream, File, FileOutputStream, ObjectOutputStream}
+import java.io.File
 
 import info.bliki.wiki.filter.PlainTextConverter
 import info.bliki.wiki.model.WikiModel
-import org.apache.spark.sql.{Dataset, Encoders, SparkSession}
-import io.github.nwtgck.wikipedia_dump_loader.{Page, Redirect, Revision, WikipediaDumpLoader}
+import io.github.nwtgck.wikipedia_dump_loader.{Page, WikipediaDumpLoader}
 import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Dataset, Encoders, SparkSession}
+import org.apache.spark.storage.StorageLevel
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -13,6 +14,10 @@ object Main {
 
     // Get Wikipedia Dump XML Path
     val wikipediaPath: String = args(0)
+
+    // Limit of pages
+    // TODO: HARD CODE: This reduces the number of pages
+    val pageLimit: Int = 1000
 
     // Create spark session
     val sparkSession: SparkSession = SparkSession
@@ -25,7 +30,7 @@ object Main {
     // Import implicits
     import sparkSession.implicits._
 
-    // Parquet Path
+    // Create a Parquet directory path
     val parquetPath: String = {
       val xmlFileName: String = new File(wikipediaPath).getName
       s"out/${xmlFileName}.ds.parquet"
@@ -44,6 +49,7 @@ object Main {
     // Convert Dataset[Page] to plain text
     val plainWikiDs: Dataset[String] =
       pageDs
+        .persist(StorageLevel.DISK_ONLY)
         .map{page =>
           for{
             wikiText <- page.revision.textOpt
@@ -51,18 +57,17 @@ object Main {
         }
         .filter(_.isDefined)
         .map(_.get)
-        .limit(1000) // TODO: HARD CODE: This reduces the number of pages
-        .cache()
+        .limit(pageLimit)
 
 
-    println(s"plainWikiDs.cout: ${plainWikiDs.count()}")
+    println(s"plainWikiDs.count: ${plainWikiDs.count()}")
 
     // Create Words RDD
     val wordsRdd: RDD[Seq[String]] =
       plainWikiDs
         .rdd
-        .map(text => text.split(" ").toSeq)
-        .cache()
+        .persist(StorageLevel.DISK_ONLY)
+        .map(text => text.toLowerCase.replaceAll("[^a-zA-Z0-9\\s]", "").split(" ").toSeq) // (from: https://github.com/snowplow/spark-example-project/blob/3729ce01f01ce2024694e7622fc5a30d58d967bf/src/main/scala/com/snowplowanalytics/spark/WordCount.scala#L47)
 
     // Create word2vec
     val word2vec: Word2Vec = new Word2Vec()
@@ -70,6 +75,7 @@ object Main {
     // Learn
     val word2VecModel: Word2VecModel = word2vec.fit(wordsRdd)
 
+    // Print synonyms to stdout
     def printSynonyms(word: String): Unit = {
       println(s"==== Synonym of '${word}' ====")
       for(synonym <- word2VecModel.findSynonyms(word, 10)){
@@ -80,8 +86,8 @@ object Main {
     printSynonyms("the")
     printSynonyms("it")
     printSynonyms("of")
-    printSynonyms("America")
-    printSynonyms("Obama")
+    printSynonyms("america")
+    printSynonyms("obama")
 
 
 
@@ -119,7 +125,7 @@ object Main {
         sparkSession,
         filePath = wikipediaPath
       )
-      // Write the Dataset to output
+      // Write the Dataset to output directory
       pageDs
         .write
         .parquet(parquetPath)
